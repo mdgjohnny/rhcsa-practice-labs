d1e19e4fa07c022a057561d5fc40981c0c01ff43
mdgjohnny <mdgjohnny@gmail.com>
2026-01-04 18:09:48 -0300

    Fix task-03: explicit hostname, use SSH to check remote hostname

--- checks/task-03.sh
+++ checks/task-03.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-# Task: Set hostname on node1
+# Task: Set hostname to rhcsa1 on node1
 # Category: networking
+# Target: node1
 
-LOCAL_HOSTNAME=$(hostname -s)
-
-check '[[ "$LOCAL_HOSTNAME" == "$NODE1" ]]' \
-    "Hostname set to $NODE1" \
-    "Hostname not set to $NODE1 (got $LOCAL_HOSTNAME)"
+check 'run_ssh "$NODE1_IP" "hostname -s" 2>/dev/null | grep -q "^rhcsa1$"' \
+    "Hostname set to rhcsa1" \
+    "Hostname not set to rhcsa1"
